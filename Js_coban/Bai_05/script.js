3322d3ede13a0651e4f9de209b45d7ead85e1ea3
minhhai205 <158561042+minhhai205@users.noreply.github.com>
2024-08-18 22:32:07 +0700

    update

--- Js_coban/Bai_05/script.js
+++ Js_coban/Bai_05/script.js
@@ -2,5 +2,9 @@ var button = document.querySelector("button")
 button.onclick = function(){
     alert("hello world")
 }
-
+var a = [1, 2, 3, 4, 5];
+a.forEach(item, () => { 
+    item = item + 5;
+})
+console.log(a);
 // what is regex
